430fb5d957f7e7f9f3548e31b901bdc3a488a783
JHTA <JHTA@JHTA-PC>
2019-07-15 19:08:46 +0900

    dnd api 연습

--- WebContent/JS/dnd/upload.js
+++ WebContent/JS/dnd/upload.js
@@ -3,14 +3,41 @@ window.addEventListener("load",function(){
     var dropZone = section.querySelector(".drop-zone");
 
     dropZone.addEventListener("dragenter",function(e){
-        console.log("들어왕ㅆ니?")
-    });
-    dropZone.addEventListener("dragleave",function(e){
-        console.log("니나갔니?")
-    });
-    dropZone.addEventListener("dragover",function(e){
-        console.log("이동중")
-    });
-
+        e.preventDefault();
+       
+        console.log("들어왔냐?")
+       
+        for(var key in e.dataTransfer)
+        console.log(key);
+        
+        for(var key in e.dataTransfer.types[0])
+            console.log(e.dataTransfer.types[0]);
+        });
+        dropZone.addEventListener("dragleave",function(e){
+            e.preventDefault();
+            dropZone.classList.remove("invalide");
+            dropZone.classList.remove("valide");
+            console.log("E떠날거니")
+        });
+        dropZone.addEventListener("dragover",function(e){
+            e.preventDefault();
+            //dropZone.classList.remove("invalide")
+            console.log("이동중")
+            if(e.dataTransfer.types != "Files"){
+                //alert("잘못된것입니다.")
+                dropZone.classList.add("invalide")
+            }else{
+                dropZone.classList.add("valide")
+            }
+            //window.setInterval()
+        });
+        
+        dropZone.addEventListener("drop",function(e){
+            e.preventDefault();
+            dropZone.classList.remove("invalide");
+            dropZone.classList.remove("valide");
+            console.log("내려놨니?")
+        });
+    
     
 });
